d768774871f57127a1ca32d405a11a6f62a53b83
J <pearson.j.james@gmail.com>
2021-02-24 22:58:05 +0000

    Adding trend splitting

--- PyTechnicalIndicators/Chart_Patterns/trends.py
+++ PyTechnicalIndicators/Chart_Patterns/trends.py
@@ -6,7 +6,6 @@ from .pits import get_pits
 
 
 def get_trend(p):
-    print(p)
     p_diff = []
     for p_index in range(len(p)):
         if p_index != len(p)-1:
@@ -17,7 +16,6 @@ def get_trend(p):
 
 
 def get_peak_trend(prices):
-    print(prices)
     peaks_list = get_peaks(prices)
     return get_trend(peaks_list)
 
@@ -45,16 +43,57 @@ def get_trend_angle(price_a, index_a, price_b, index_b):
 
 
 def break_down_trends(typical_prices, min_period=2):
-    peaks_list = get_peaks(typical_prices)
-    pits_list = get_pits(typical_prices)
+    peaks_list = get_peaks(typical_prices, min_period)
+    pits_list = get_pits(typical_prices, min_period)
 
-    for price_index in range(min_period, len(typical_prices)):
-        # TODO: go through each, one after the other to detrmine the trend between a and b, if the trend between c and b
-        #  deviates by x amount then it is a new trend otherwise carry one until it changes.
-        #  to graph see draw a line from the last peak/pit based on the trend, so if trend is 1.5 then for each one away
-        #  from the peak/pit add 1.5 to get the limit, add +- 2 stddev to see how far it could go
-        #  taking into account the diff between peaks, and pits will allow to stop false positives
-        #  this will need to be graphed to see how applicable it is...
+    peak_trends = []
+    pit_trends = []
+
+    # TODO: take into account the index diff between last two and apply trend to figure out what the actual stddev diff is
+
+    # TODO: could export into a diff function and call it with the different peak/pit values
+    previous_peak_trend = None
+    peak_start = ()
+    for peak_position in range(len(peaks_list)-1):
+        current_trend = peaks_list[peak_position+1] - peaks_list[peak_position]
+
+        if previous_peak_trend is None:
+            previous_peak_trend = current_trend
+            peak_start = peaks_list[peak_position]
+            continue
+
+        stddev_diff = 2*(statistics.stdev(previous_peak_trend))
+
+        if current_trend >= previous_peak_trend+stddev_diff or current_trend <= previous_peak_trend-stddev_diff:
+            period_trend = peaks_list[peak_position][0] - peak_start[0]
+            peak_trends.append((peak_start[1], peaks_list[peak_position][1], period_trend))
+            peak_start = peaks_list[peak_position]
+
+        previous_peak_trend = current_trend
+
+    previous_pit_trend = None
+    pit_start = ()
+    for pit_position in range(len(pits_list)-1):
+        current_trend = pits_list[pit_position+1] - pits_list[pit_position]
+
+        if previous_pit_trend is None:
+            previous_pit_trend = current_trend
+            pit_start = pits_list[pit_position]
+            continue
+
+        stddev_diff = 2*(statistics.stdev(previous_pit_trend))
+
+        if current_trend >= previous_pit_trend+stddev_diff or current_trend <= previous_pit_trend-stddev_diff:
+            period_trend = pits_list[pit_position][0] - pit_start[0]
+            pit_trends.append((peak_start[1], peaks_list[pit_position][1], period_trend))
+            pit_start = pits_list[pit_position]
+
+        previous_pit_trend = current_trend
+
+
+    # TODO: figure out how to merge...
+    #  take into account the diff between peaks, and pits will allow to stop false positives
+    #  this will need to be graphed to see how applicable it is...
 
 
 # TODO: figure out the trendline that connects the most peaks/pits so that breakouts can be highlighted
