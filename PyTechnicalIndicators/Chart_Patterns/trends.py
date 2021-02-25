e82de9f768cc634d139c13d1b9a69614b84be8fc
J <pearson.j.james@gmail.com>
2021-02-25 22:21:06 +0000

    Added trend breakdowns

--- PyTechnicalIndicators/Chart_Patterns/trends.py
+++ PyTechnicalIndicators/Chart_Patterns/trends.py
@@ -42,54 +42,53 @@ def get_trend_angle(price_a, index_a, price_b, index_b):
     return math.degrees(angle)
 
 
+def break_down_trend(price_list):
+    trends = []
+
+    previous_trend = None
+    previous_tuple = ()
+    trend_start = ()
+
+    for position in range(len(price_list)-1):
+        current_trend = price_list[position+1][0] - price_list[position][0]
+
+        if previous_trend is None:
+            previous_trend = current_trend
+            previous_price = price_list[position]
+            trend_start = price_list[position]
+            continue
+
+        current_price = price_list[position][0]
+        current_index = price_list[position][1]
+        previous_price = previous_tuple[0]
+        previous_index = previous_tuple[1]
+        time_passed = current_index - previous_index
+        trended_price = previous_price + (previous_trend * time_passed)
+
+        stddev_diff = 2*(statistics.stdev(trended_price))
+
+        upper_trend_limit = trended_price + stddev_diff
+        lower_trend_limit = trended_price - stddev_diff
+
+        if current_trend > upper_trend_limit or current_trend < lower_trend_limit:
+            period_trend = current_price - trend_start[0]
+            trends.append((trend_start[1], current_index, period_trend))
+            trend_start = price_list[position]
+
+        previous_tuple = price_list[position]
+        previous_trend = current_trend
+
+    return trends
+
+
 def break_down_trends(typical_prices, min_period=2):
     peaks_list = get_peaks(typical_prices, min_period)
     pits_list = get_pits(typical_prices, min_period)
 
-    peak_trends = []
-    pit_trends = []
+    peak_trends = break_down_trend(peaks_list)
+    pit_trends = break_down_trend(pits_list)
 
-    # TODO: take into account the index diff between last two and apply trend to figure out what the actual stddev diff is
-
-    # TODO: could export into a diff function and call it with the different peak/pit values
-    previous_peak_trend = None
-    peak_start = ()
-    for peak_position in range(len(peaks_list)-1):
-        current_trend = peaks_list[peak_position+1] - peaks_list[peak_position]
-
-        if previous_peak_trend is None:
-            previous_peak_trend = current_trend
-            peak_start = peaks_list[peak_position]
-            continue
-
-        stddev_diff = 2*(statistics.stdev(previous_peak_trend))
-
-        if current_trend >= previous_peak_trend+stddev_diff or current_trend <= previous_peak_trend-stddev_diff:
-            period_trend = peaks_list[peak_position][0] - peak_start[0]
-            peak_trends.append((peak_start[1], peaks_list[peak_position][1], period_trend))
-            peak_start = peaks_list[peak_position]
-
-        previous_peak_trend = current_trend
-
-    previous_pit_trend = None
-    pit_start = ()
-    for pit_position in range(len(pits_list)-1):
-        current_trend = pits_list[pit_position+1] - pits_list[pit_position]
-
-        if previous_pit_trend is None:
-            previous_pit_trend = current_trend
-            pit_start = pits_list[pit_position]
-            continue
-
-        stddev_diff = 2*(statistics.stdev(previous_pit_trend))
-
-        if current_trend >= previous_pit_trend+stddev_diff or current_trend <= previous_pit_trend-stddev_diff:
-            period_trend = pits_list[pit_position][0] - pit_start[0]
-            pit_trends.append((peak_start[1], peaks_list[pit_position][1], period_trend))
-            pit_start = pits_list[pit_position]
-
-        previous_pit_trend = current_trend
-
+    return peak_trends, pit_trends
 
     # TODO: figure out how to merge...
     #  take into account the diff between peaks, and pits will allow to stop false positives
